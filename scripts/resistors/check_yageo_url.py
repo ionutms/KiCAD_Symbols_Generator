99a4c6c807a0274388662afd082a5c7b868e7af8
ionutms <mihai.ionut.career@gmail.com>
2024-12-22 20:45:54 +0200

    fix: update MPN prefix in check_yageo_url script for accuracy

--- scripts/resistors/check_yageo_url.py
+++ scripts/resistors/check_yageo_url.py
@@ -125,7 +125,7 @@ def check_url(
                 f"at search number {index}")
 
 if __name__ == "__main__":
-    mpn_prefixes = ["RT0805CRE07"]
+    mpn_prefixes = ["RT0805DRE07"]
 
     for mpn_prefix in mpn_prefixes:
         file_path = f"data/{mpn_prefix}_part_numbers.csv"
