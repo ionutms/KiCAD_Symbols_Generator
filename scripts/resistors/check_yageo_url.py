5e4d8bec3daa73d1e107e20b6284b89d52686d33
ionutms <mihai.ionut.career@gmail.com>
2024-12-23 19:45:36 +0200

    fix: update MPN prefix in check_yageo_url script to RC0805BR-07

--- scripts/resistors/check_yageo_url.py
+++ scripts/resistors/check_yageo_url.py
@@ -125,7 +125,7 @@ def check_url(
                 f"at search number {index}")
 
 if __name__ == "__main__":
-    mpn_prefixes = ["RC0603FR-07"]
+    mpn_prefixes = ["RC0805BR-07"]
 
     for mpn_prefix in mpn_prefixes:
         file_path = f"data/{mpn_prefix}_part_numbers.csv"
