d83aa2b981b230ce64e3e6e66bb5ee06cb1c3108
ionutms <mihai.ionut.career@gmail.com>
2025-01-06 11:25:22 +0200

    minor updates

--- scripts/connectors/symbol_connectors_specs.py
+++ scripts/connectors/symbol_connectors_specs.py
@@ -357,7 +357,7 @@ SYMBOLS_SPECS: dict[str, SeriesSpec] = {
             "_ga*MTYxNDYyMTQ0Mi4xNzM2MDk5MTUz*"
             "_ga_3KFNZC07WW*MTczNjA5OTE1My4xLjEuMTczNjEwMDY3My4zMC4wLjA."
         ),
-        pin_counts=list(range(1, 11)),
+        pin_counts=list(range(1, 21)),
         trustedparts_link="https://www.trustedparts.com/en/search",
         color="Black",
         pitch=1.27,
