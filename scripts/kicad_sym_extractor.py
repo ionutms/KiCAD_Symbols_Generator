6e5747a1493d8361164a9449d90a75b684f409e7
ionutms <mihai.ionut.career@gmail.com>
2025-02-18 09:56:33 +0200

    Add Bosch IMU and pressure sensor data to UNITED_IC_BOSCH.csv and update extractor script

--- scripts/kicad_sym_extractor.py
+++ scripts/kicad_sym_extractor.py
@@ -88,6 +88,7 @@ if __name__ == "__main__":
     input_files = [
         "UNITED_IC_ADI.kicad_sym",
         "UNITED_IC_TI.kicad_sym",
+        "UNITED_IC_BOSCH.kicad_sym",
         "UNITED_SOLDER_JUMPERS.kicad_sym",
         "UNITED_TEST_POINTS.kicad_sym",
         "UNITED_MODULES_DATA_BASE.kicad_sym",
