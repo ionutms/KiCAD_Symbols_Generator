7da2eef98fc379f869f9beed87ccf29a8021070f
ionutms <mihai.ionut.career@gmail.com>
2025-03-04 11:10:37 +0200

    Add alternative right pin configurations for coupled inductor symbols

--- scripts/coupled_inductors/symbol_coupled_inductors_specs.py
+++ scripts/coupled_inductors/symbol_coupled_inductors_specs.py
@@ -45,6 +45,7 @@ class SidePinConfig(NamedTuple):
 
     left: list[PinConfig]
     right: list[PinConfig]
+    right_alternative: list[PinConfig]
 
 
 class SeriesSpec(NamedTuple):
@@ -80,7 +81,6 @@ class SeriesSpec(NamedTuple):
     max_dc_current: list[float]
     max_dc_resistance: list[float]
     pin_config: SidePinConfig
-    reversed_polarity_symbol: bool = True
     reference: str = "L"
 
 
@@ -338,7 +338,6 @@ SYMBOLS_SPECS: dict[str, SeriesSpec] = {
         ],
         value_suffix="ML",
         trustedparts_link="https://www.trustedparts.com/en/search",
-        reversed_polarity_symbol=False,
         pin_config=SidePinConfig(
             left=[
                 PinConfig("1", 5.08, "unspecified", 5.08),
@@ -347,6 +346,10 @@ SYMBOLS_SPECS: dict[str, SeriesSpec] = {
             right=[
                 PinConfig("2", 5.08, "unspecified", 5.08),
                 PinConfig("4", -5.08, "unspecified", 5.08),
+            ],
+            right_alternative=[
+                PinConfig("2", -5.08, "unspecified", 5.08),
+                PinConfig("4", 5.08, "unspecified", 5.08),
             ],
         ),
     ),
@@ -364,7 +367,6 @@ SYMBOLS_SPECS: dict[str, SeriesSpec] = {
         max_dc_resistance=[0.053, 0.098, 0.208, 0.297, 0.387],
         value_suffix="ME",
         trustedparts_link="https://www.trustedparts.com/en/search",
-        reversed_polarity_symbol=True,
         pin_config=SidePinConfig(
             left=[
                 PinConfig("1", 5.08, "unspecified", 5.08),
@@ -373,6 +375,10 @@ SYMBOLS_SPECS: dict[str, SeriesSpec] = {
             right=[
                 PinConfig("4", 5.08, "unspecified", 5.08),
                 PinConfig("2", -5.08, "unspecified", 5.08),
+            ],
+            right_alternative=[
+                PinConfig("4", -5.08, "unspecified", 5.08),
+                PinConfig("2", 5.08, "unspecified", 5.08),
             ],
         ),
     ),
@@ -399,7 +405,6 @@ SYMBOLS_SPECS: dict[str, SeriesSpec] = {
         ],
         value_suffix="ML",
         trustedparts_link="https://www.trustedparts.com/en/search",
-        reversed_polarity_symbol=False,
         pin_config=SidePinConfig(
             left=[
                 PinConfig("1", 5.08, "unspecified", 5.08),
@@ -408,6 +413,10 @@ SYMBOLS_SPECS: dict[str, SeriesSpec] = {
             right=[
                 PinConfig("2", 5.08, "unspecified", 5.08),
                 PinConfig("4", -5.08, "unspecified", 5.08),
+            ],
+            right_alternative=[
+                PinConfig("2", -5.08, "unspecified", 5.08),
+                PinConfig("4", 5.08, "unspecified", 5.08),
             ],
         ),
     ),
