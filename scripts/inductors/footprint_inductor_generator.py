a672f876695bb9608cb45ee0576fcd99438a2269
ionutms <mihai.ionut.career@gmail.com>
2025-03-18 08:50:46 +0200

    Remove redundant 3D model association in inductor footprint generator

--- scripts/inductors/footprint_inductor_generator.py
+++ scripts/inductors/footprint_inductor_generator.py
@@ -50,11 +50,6 @@ def generate_footprint(
         footprint_utils.generate_pin_1_indicator(pad_center_x, pad_width),
         footprint_utils.generate_pads(pad_width, pad_height, pad_center_x),
         footprint_utils.associate_3d_model(
-            "${KIPRJMOD}/KiCAD_Symbol_Generator/3D_models",
-            part_info.series,
-            hide=True,
-        ),
-        footprint_utils.associate_3d_model(
             "${3D_MODELS_VAULT}/3D_models/inductors",
             part_info.series,
         ),
