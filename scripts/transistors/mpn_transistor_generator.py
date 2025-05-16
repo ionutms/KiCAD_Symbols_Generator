b49f9d7dc645b919f6d7f7792c9c1d30db1155c7
ionutms <mihai.ionut.career@gmail.com>
2025-05-16 13:32:00 +0300

    Remove 3D viewer link from header mapping in transistor generator

--- scripts/transistors/mpn_transistor_generator.py
+++ scripts/transistors/mpn_transistor_generator.py
@@ -60,7 +60,6 @@ HEADER_MAPPING: Final[dict] = {
     "Trustedparts Search": lambda part: part.trustedparts_link,
     "Drain Current (A)": lambda part: f"{part.drain_current:.1f}",
     "Transistor Type": lambda part: part.transistor_type,
-    "3dviewer Link": lambda part: part.step_file_viewer_link,
 }
 
 
