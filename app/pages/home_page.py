30edf4524e17547be8cd7994ce595b3749f2f53e
ionutms <mihai.ionut.career@gmail.com>
2026-01-19 21:41:00 +0200

    Update home page

--- app/pages/home_page.py
+++ app/pages/home_page.py
@@ -250,7 +250,11 @@ def create_project_links(
         links.append(
             html.A(
                 "LTC3350 Analysis (Binder)",
-                href="https://mybinder.org/v2/gh/ionutms/LTC3350_Interactive_Calculator/main?urlpath=%2Fdoc%2Ftree%2FLTC3350_Analysis.ipynb",
+                href=(
+                    "https://mybinder.org/v2/gh/ionutms/"
+                    "LTC3350_Interactive_Calculator/"
+                    "main?urlpath=%2Fdoc%2Ftree%2FLTC3350_Analysis.ipynb"
+                ),
                 target="_blank",
             )
         )
