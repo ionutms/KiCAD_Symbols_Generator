e7cc08097ddd3c3f705bd14b573368ee80139314
ionutms <mihai.ionut.career@gmail.com>
2026-04-07 21:25:42 +0300

    Add connector specs for Molex 1600133623

--- scripts/connectors/symbol_connectors_specs.py
+++ scripts/connectors/symbol_connectors_specs.py
@@ -1301,4 +1301,23 @@ SYMBOLS_SPECS: dict[str, SeriesSpec] = {
         mounting_style="Through Hole",
         contact_plating="Tin",
     ),
+    "1600133623": SeriesSpec(
+        manufacturer="Molex",
+        base_series="1600133623",
+        footprint_pattern="connector_footprints:1600133623",
+        datasheet=(
+            "https://www.molex.com/en-us/products/part-detail/"
+            "1600133623?display=pdf"
+        ),
+        pin_counts=[56],
+        trustedparts_link="https://www.trustedparts.com/en/search",
+        color="Gray",
+        pitch=2.0,
+        number_of_rows=1,
+        mounting_angle="Right Angle",
+        current_rating=4,
+        voltage_rating=60,
+        mounting_style="Through Hole",
+        contact_plating="Tin",
+    ),
 }
