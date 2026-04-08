724026600b802f7bd248ddca7c68d68fc6a9459d
ionutms <mihai.ionut.career@gmail.com>
2026-04-08 21:23:18 +0300

    Add 1600133741 connector footprint and symbol specifications

--- scripts/connectors/symbol_connectors_specs.py
+++ scripts/connectors/symbol_connectors_specs.py
@@ -1358,4 +1358,23 @@ SYMBOLS_SPECS: dict[str, SeriesSpec] = {
         mounting_style="Through Hole",
         contact_plating="Tin",
     ),
+    "1600133741": SeriesSpec(
+        manufacturer="Molex",
+        base_series="1600133741",
+        footprint_pattern="connector_footprints:1600133741",
+        datasheet=(
+            "https://www.molex.com/en-us/products/part-detail/"
+            "1600133741?display=pdf"
+        ),
+        pin_counts=[56],
+        trustedparts_link="https://www.trustedparts.com/en/search",
+        color="Gray",
+        pitch=2.0,
+        number_of_rows=1,
+        mounting_angle="Right Angle",
+        current_rating=4,
+        voltage_rating=60,
+        mounting_style="Through Hole",
+        contact_plating="Tin",
+    ),
 }
